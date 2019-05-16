48eed79890807ebb7cfca296c2d4c786ff6540e6
renato.melo <renatoredes@live.com>
2019-05-16 16:23:45 -0300

    Front-End Services-Professional- Angular

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'employee', pathMatch: 'full' },
+  { path: 'employees', component: EmployeeListComponent },
+  { path: 'add', component: CreateEmployeeComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/employee-details/employee-details.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Employee } from '../employee';
+import {EmployeeService} from '../employee.service';
+import {EmployeeListComponent} from '../employee-list/employee-list.component';
+
+
+@Component({
+  selector: 'app-employee-details',
+  templateUrl: './employee-details.component.html',
+  styleUrls: ['./employee-details.component.css']
+})
+export class EmployeeDetailsComponent implements OnInit {
+
+  @Input() employee: Employee
+
+  constructor(private employeeService: EmployeeService, private listComponent: EmployeeListComponent) { }
+
+  ngOnInit() {
+  }
+
+}
